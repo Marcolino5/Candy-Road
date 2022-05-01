2ad9506b438460edd031d59892113943b218401b
Marcolino5 <marcos.alex2004@gmail.com>
2022-05-01 18:30:51 -0300

    Fixes

--- main.s
+++ main.s
@@ -4,8 +4,13 @@
 .include "Imagens/Menu.s"
 .include "Imagens/Course1.s"
 .include "Imagens/Course2.s"
+.include "Imagens/CharacterSelection.s"
+.include "Imagens/Lamar.s"
+.include "Imagens/Selected.s"
+.include "Imagens/Unselected.s"
 
 .include "Imagens/CarroVM.s"
+.include "Imagens/CarroSM.s"
 .include "Imagens/Map.s"
 
 .include "Imagens/Acelerador1.s"
@@ -25,7 +30,7 @@
 .include "Musicas/MusicaMenu.s"
 .include "Musicas/Largada1.s"
 .include "Musicas/MusicaLargada1.s"
-.include "Musicas/Naruto.s"
+.include "Musicas/Emotion.s"
 .include "Musicas/MusicaVitoria.s"
 
 ### TILES ###
@@ -44,6 +49,7 @@
 
 ### CARROS ###
 .include "Imagens/CarroV0.data"
+.include "Imagens/CarroS0.s"
 
 TEMPOACELERADOR: .word 0x00000000
 PONTUAÇÃO: .word 0x00000000
@@ -52,6 +58,14 @@ PONTUAÇÃOFASE2: .word 0x00000000
 TEMPOGASOLINA: .word 0x00000000
 CONTADORGASOLINA: .word 0x00000000
 POSIÇÃOCARROM: .word 0xFF011584
+CARRO: .word 0x00000000
+CARROM: .word 0x00000000
+UNLOCKED: .byte 0
+
+NIMAGENS: .word 0x00000000
+MAPAATUAL: .word 0x00000000
+NLOOP: .word 0x00000000
+RAPRINT: .word 0x00000000
 
 .text
 j MENU	
@@ -101,7 +115,8 @@ menuMUSICA:
 		beq s10, t1, goMAPA1
 		j SETA # Se não tiver no endereço, volta p/ SETA
 		
-	goMAPA1:beq t0, a1, COURSE1
+	goMAPA1:li s11, 0xFF002E71
+		beq t0, a1, CHARACTERSELECTION1
 		
 		j SETA # Se não tiver apertado, volta p/ SETA
 	SETAU:  
@@ -130,10 +145,13 @@ menuMUSICA:
 		li t6, 9600
 		add s10, s10, t6
 		j SETA
-	li a7, 10
-	ecall
 	
 COURSE1:
+	la t1, TEMPOACELERADOR
+	li a7, 30
+	ecall
+	sw a0, (t1)			# Coloca tempo inicial no TEMPOACELERADOR
+	
 	la a0, Course1
 	call PRINT
 	
@@ -142,6 +160,122 @@ COURSE1:
 	ecall
 	
 	j FASE1
+CHARACTERSELECTION1:
+	la a0, CharacterSelection
+	call PRINT
+	ESCOLHA1:
+		beqz s7, playMUSICA2  # Se contador = 0, entra na música direto
+		li a7, 30	     
+		ecall		     # Chama syscall TIME
+		blt a0, s8, escolha1CONT # Compara os dois tempos
+		addi s0,s0,8	     # Se a0 >= s8, continua a tocar
+	playMUSICA2:
+		call tocamusicaCS
+	escolha1CONT:
+		add t2, s11, zero
+		la a1, Selected
+		li a4, 72			# Define largura da imagem
+		li a6, 88			# Define altura da imagem
+		call printUND
+	
+		li t5, 0xFF200000 # Carrega endereço do KDMMIO
+		lw t0, 4(t5)      # Lê código ASCII da tecla
+		sw t0, 12(t5)     # Põe no display
+		sw zero, 4(t5)    # Limpa o código ASCII
+	
+		li t1, 87         # Código ASCII do W
+		beq t1, t0, SELECTEDW1
+		li t1, 119	  # Código ASCII do w
+		beq t1, t0, SELECTEDW1
+	
+ 		li t1, 83         # Código ASCII do S
+ 		beq t1, t0, SELECTEDS1
+		li t1, 115        # Código ASCII do s
+		beq t1, t0, SELECTEDS1
+	
+		li t1, 65	  # Código ASCII do A
+		beq t1, t0, SELECTEDA1
+		li t1, 97	  # Código ASCII do a
+		beq t1, t0, SELECTEDA1
+	
+		li t1, 68	  # Código ASCII do D
+		beq t1, t0, SELECTEDD1
+		li t1, 100	  # Código ASCII do d
+		beq t1, t0, SELECTEDD1
+	
+		li t1, 32         # Código ASCII do espaço
+		beq t1, t0, SELECTEDSPACE1
+	
+		j ESCOLHA1
+		SELECTEDW1:
+			li t1, 0xFF002E71
+			beq t1, s11, ESCOLHA1
+			li t1, 0xFF002EF9
+			beq t1, s11, ESCOLHA1		# Limita a movimentação
+			
+			add t2, s11, zero
+			la a1, Unselected
+			li a4, 72			# Define largura da imagem
+			li a6, 88			# Define altura da imagem
+			call printUND			# Limpa rastro
+			
+	   		li t1, -31680
+	   		add s11, t1, s11
+	   		j ESCOLHA1
+		SELECTEDS1:
+			li t1, 0xFF00AA31
+			beq t1, s11, ESCOLHA1
+			li t1, 0xFF00AAB9
+			beq t1, s11, ESCOLHA1		# Limita a movimentação
+		
+			add t2, s11, zero
+			la a1, Unselected
+			li a4, 72			# Define largura da imagem
+			li a6, 88			# Define altura da imagem
+			call printUND			# Limpa rastro
+			
+			li t1, 31680
+			add s11, t1, s11
+			j ESCOLHA1	
+		SELECTEDA1:
+			li t1, 0xFF002E71
+			beq t1, s11, ESCOLHA1
+			li t1, 0xFF00AA31
+			beq t1, s11, ESCOLHA1		# Limita a movimentação
+			
+			add t2, s11, zero
+			la a1, Unselected
+			li a4, 72			# Define largura da imagem
+			li a6, 88			# Define altura da imagem
+			call printUND			# Limpa rastro
+			
+			addi s11, s11, -136
+			j ESCOLHA1
+		SELECTEDD1:
+			li t1, 0xFF002EF9
+			beq t1, s11, ESCOLHA1
+			li t1, 0xFF00AAB9
+			beq t1, s11, ESCOLHA1           # Limita a movimentação
+			
+			add t2, s11, zero
+			la a1, Unselected
+			li a4, 72			# Define largura da imagem
+			li a6, 88			# Define altura da imagem
+			call printUND			# Limpa rastro
+			
+			addi s11, s11, 136
+			j ESCOLHA1
+		SELECTEDSPACE1:
+			li t1, 0xFF002E71
+			beq s11, t1, VANELLOPE1		# Escolheu Vanellope
+			li t1, 0xFF002EF9
+			beq s11, t1, GLOYD1		# Escolheu Gloyd
+			li t1, 0xFF00AA31
+			beq s11, t1, SNOWANNA1		# Escolheu Snowanna
+			lb t1, UNLOCKED
+			beqz t1, ESCOLHA1
+			li t1, 0xFF00AAB9
+			beq s11, t1, LAMAR1		# Escolheu Lamar	
 FASE1:
 	li s10, 0xFF00FE30       	# Define inicio p/ MoveCARRO - NÃO USAR REGISTRADOR EM OUTRAS PARTES
 	li s11, 0                       # Contador da aceleração - NÃO USAR REGISTRADOR EM OUTRAS PARTES
@@ -165,7 +299,7 @@ FASE1:
 	call printUND
 	
 	li t2, 0xFF111584
-	la a1, CarroVM
+	lw a1, CARROM
 	li a4, 8			# Define largura da imagem
 	li a6, 16			# Define altura da imagem
 	call printUND
@@ -183,7 +317,7 @@ FASE1:
 	
 	add t2, s10, zero  		# Coloca valor armazenado em s10 em t2
 	add a3, t2, zero  		# Coloca valor armazenado em t2 em a3
-	la a1, CarroV0
+	lw a1, CARRO
 	li a4, 16        		# Define largura da imagem
 	li a6, 16			# Define altura da imagem
 	call printUND
@@ -201,7 +335,7 @@ FASE1:
 	call printUND
 	
 	li t2, 0xFF011584
-	la a1, CarroVM
+	lw a1, CARROM
 	li a4, 8			# Define largura da imagem
 	li a6, 16			# Define altura da imagem
 	call printUND
@@ -217,38 +351,70 @@ FASE1:
 	li s7, 0			# Inicia contador de notas
 	call LARGADA                    # Chama a função LARGADA
 	
-	la s3, Mapa1                    # Carrega Mapa1
-        li s4, 9                        # Define quantas imagens 320x240 de Mapa1 serão printadas
+	la t1, MAPAATUAL
+	la t2, Mapa1                    # Carrega Mapa1
+	sw t2, (t1)			# Coloca endereço de Mapa1 em MAPAATUAL
+	la t1, NIMAGENS
+	li t2, 9
+	sw t2, (t1)			# Define quantas imagens 320x240 de Mapa1 serão printadas
       	call printSEQUENCE	        # Chama função PRINT-SEQUENCE
       	
-	li s8, 24                        # Define quantas vezes o loop será chamado
+      	la t1, NLOOP
+      	li t2, 24
+      	sw t2, (t1)			# Define quantas vezes o loop será chamado
   	MAPA1.2_LOOP:
-      		la s3, Mapa2            # Carrega Mapa2
-      		li s4, 8    		# Define quantas imagens 320x240 de Mapa2 serão printadas 
+      		la t1, MAPAATUAL
+		la t2, Mapa2                    # Carrega Mapa2
+		sw t2, (t1)			# Coloca endereço de Mapa1 em MAPAATUAL
+      		la t1, NIMAGENS
+		li t2, 8
+		sw t2, (t1)		# Define quantas imagens 320x240 de Mapa1 serão printadas
       		call printSEQUENCE
       		
-      		addi s8, s8, -1
-      		bnez s8, MAPA1.2_LOOP
+      		la t1, NLOOP
+      		lw t2, NLOOP
+      		addi t2, t2, -1
+      		sw t2, (t1)		# Atualiza o valor de NLOOP
+      		bnez t2, MAPA1.2_LOOP
       	
-      	la s3, Mapa2.5			# Carrega Mapa2.5
-      	li s4, 8			# Define quantas imagens 320x240 de Mapa2.4 serão printadas
+      	la t1, MAPAATUAL
+	la t2, Mapa2.5                  # Carrega Mapa2.5
+	sw t2, (t1)			# Coloca endereço de Mapa1 em MAPAATUAL
+      	la t1, NIMAGENS
+	li t2, 8
+	sw t2, (t1)			# Define quantas imagens 320x240 de Mapa1 serão printadas
       	call printSEQUENCE
       	
-      	la s3, Mapa2.55			# Carrega Mapa2.55
-      	li s4, 7			# Define quantas imagens 320x240 de Mapa2.55 serão printadas
+      	la t1, MAPAATUAL
+	la t2, Mapa2.55                 # Carrega Mapa2.55
+	sw t2, (t1)			# Coloca endereço de Mapa1 em MAPAATUAL
+      	la t1, NIMAGENS
+	li t2, 7
+	sw t2, (t1)			# Define quantas imagens 320x240 de Mapa1 serão printadas
       	call printSEQUENCE
       	
-      	li s8, 24                        # Define quantas vezes o loop será chamado
+      	la t1, NLOOP
+      	li t2, 24
+      	sw t2, (t1)			# Define quantas vezes o loop será chamado
   	MAPA1.3_LOOP:
-      		la s3, Mapa3            # Carrega Mapa2
-      		li s4, 8    		# Define quantas imagens 320x240 de Mapa2 serão printadas 
+      		la t1, MAPAATUAL
+		la t2, Mapa3                    # Carrega Mapa3
+		sw t2, (t1)			# Coloca endereço de Mapa1 em MAPAATUAL
+      		la t1, NIMAGENS
+		li t2, 8
+		sw t2, (t1)			# Define quantas imagens 320x240 de Mapa1 serão printadas
       		call printSEQUENCE
       		
-      		addi s8, s8, -1
-      		bnez s8, MAPA1.3_LOOP
+      		la t1, NLOOP
+      		lw t2, NLOOP
+      		addi t2, t2, -1
+      		sw t2, (t1)			# Atualiza o valor de NLOOP
+      		bnez t2, MAPA1.3_LOOP
       	
       	la s3, Vitoria1
-      	li s4, 8
+      	la t1, NIMAGENS
+	li t2, 8
+	sw t2, (t1)			# Define quantas imagens 320x240 de Mapa1 serão printadas
       	call printSEQUENCE
       	
       	j VITORIA1
@@ -281,16 +447,18 @@ PRINT:
 
 # Printa imagens 320x240 em sequência, apresenta função MOVE dentro
 # >Argumentos: a1 (n de imagens)<
-printSEQUENCE: 
-	mv s0, ra                               # Salva endereço de retorno
+printSEQUENCE:
+	la t1, RAPRINT
+	sw ra, (t1) 				# Salva endereço de retorno
 			
 	printSEQUENCE_LOOP:
 		li t2,0xFF00001C
 		li a4,216
 		li a6,240
-		mv a1,s3
+		lw a1,MAPAATUAL			# Carrega endereço que está em MAPAATUAL
 		call printUND			# Printa a imagem no endereço atual, que sai no processo +76800 (320 x 240)
-		mv s3,a1
+		la t1, MAPAATUAL
+		sw a1, (t1)			# Atualiza o endereço em MAPAATUAL
 	MOVE:
 		attGASOLINA:
 			li a7, 30
@@ -313,7 +481,7 @@ printSEQUENCE:
 			lw t0, 4(t4)	   		# lê codigo ASCII da tecla
 			sw t0, 12(t4)      		# põe no display
 			sw zero, 4(t4)    		# Limpa o código ASCII
-		
+			
 			li t1, 87          		# Código ASCII do W
 			beq t0, t1, moveCONT3  		# Se código == W, pula e continua
 			li t1, 119          		# Código ASCII do w
@@ -336,7 +504,8 @@ printSEQUENCE:
 		moveCONT3:
 			lw t2, POSIÇÃOCARROM
 			li t4, 2
-			rem t5, s4, t4
+			lw t6, NIMAGENS
+			rem t5, t6, t4
 			bnez t5, moveCONT4		# Move só a cada 2 passos
 
 			li t2, 0xFF000000
@@ -350,12 +519,14 @@ printSEQUENCE:
 			add t2, t2, t1			# Coloca nova posição do carro da interface
 		moveCONT4:
 			add a7, t2, zero
-			la a1, CarroVM
+			lw a1, CARROM
 			li a4, 8			# Define largura da imagem
 			li a6, 16			# Define altura da imagem
 			call printUND
 			la t3, POSIÇÃOCARROM
 			sw a7, (t3)			# Guarda a nova posição
+			
+			
 			
 			lw t1, PONTUAÇÃO
 			addi t1, t1, 100
@@ -372,10 +543,13 @@ printSEQUENCE:
 			beq t1, t3, MOEDA4
 		moveCONT5:
 			addi s11, s11, 1
-			addi s4, s4, -1
-			bnez s4, printSEQUENCE_LOOP  	# Loop enquanto s1 != a1
+			la t1, NIMAGENS
+			lw t2, NIMAGENS
+			addi t2, t2, -1			# Diminui 1 de t2 na contagem
+			sw t2, (t1)
+			bnez t2, printSEQUENCE_LOOP  	# Loop enquanto s1 != a1
 				
-			mv ra, s0
+			lw ra, RAPRINT
 			ret
 		
 # Printa imagem com dimensões definidas fora da função
@@ -421,10 +595,9 @@ moveCARRO:
 	mv a7, ra
 	
 	add t2, s10, zero # Coloca valor armazenado em s10 em t2
-	
 	add a3, t2, zero # Coloca valor armazenado em t2 em a3
 	
-	la a1, CarroV0
+	lw a1, CARRO
 	li a4, 16	# Define largura da imagem
 	li a6, 16	# Define altura da imagem
 	call printUND
@@ -457,9 +630,8 @@ moveCARRO:
 moveCARROR:
 	li t2, 0xFF00FE70
 	beq s10, t2, PRINT_EXIT
-
-	add t2, s10, zero
 	
+	add t2, s10, zero
 	la a1, TILE1
 	li a4, 16	# Define largura
 	li a6, 16	# Define altura
@@ -471,11 +643,10 @@ moveCARROR:
 	addi s10, s10, 4
 	j moveCARRO
 moveCARROL:
-	li t2, 0xFF00FE18
+	li t2, 0xFF00FE70
 	beq s10, t2, PRINT_EXIT
 
 	add t2, s10, zero
-	
 	la a1, TILE1
 	li a4, 16	# Define largura
 	li a6, 16	# Define altura
@@ -502,14 +673,41 @@ tocamusicaMENU:
 		li a7,31	
 		ecall			# Toca a nota
 		addi s7,s7,1		# Adiciona um ao contador de notas	
-	
+		
+		mv t3, a1
 		li a7, 30		# Pega quando tocou a nota
 		ecall
-		add s8,a0,a1		# Define quando vai tocar de novo
-		addi s8,s8,-210
+		add s8,a0,t3		# Define quando vai tocar de novo
 		
 		mv ra, t2
 		ret
+
+# Toca música sem pausar o jogo
+# >Argumentos: s1 (n de notas), s0 (endereço), s7 (n de notas já tocado), a3 (Volume)<
+tocamusicaCS:
+	mv t2, ra
+	la t0,numMENU	        # Coloca n de notas em s1	
+	lw s1,(t0)	
+	li a2,2  		# Instrumento
+	li a3,42                # Volume
+	tocaCS:	
+		beq s7,s1,Restart	# Se tiver tocado todas as notas, volta p/ MENU
+		lw a0,(s0)		# Coloca nota em a0
+		lw a1,4(s0)		# Coloca duração em a1
+		li a7,31	
+		ecall			# Toca a nota
+		addi s7,s7,1		# Adiciona um ao contador de notas	
+		
+		mv t3, a1
+		li a7, 30		# Pega quando tocou a nota
+		ecall
+		add s8,a0,t3		# Define quando vai tocar de novo
+		
+		mv ra, t2
+		ret
+Restart:	la s0, notasMENU
+		li s7,0			# Reinicia contador de notas
+		j ESCOLHA1
 		
 # Toca a primeira música e um som 4 vezes pausadamente, sendo a largada da corrida
 # >Argumentos: s0 (Notas da Largada) e s7 (Contador de Notas)<
@@ -627,6 +825,7 @@ PRINTAGASOLINA:
 	bge t1, t5, DERROTA
 	
 	li a7, 30
+	ecall
 	li t3, 0x00001D4C
 	add t4, a0, t3
 	la t1, TEMPOGASOLINA		# Altera o tempo de mudança da gasolina
@@ -662,11 +861,11 @@ DERROTA:
 	li a6, 16
 	call printUND			# Coloca imagem de final de jogo
 	
-	la t1,numNARUTO			
+	la t1,numEMOTION			
 	lw t2,0(t1)		
-	la t1,notasNARUTO		
+	la t1,notasEMOTION		
 	li t0,0				# Inicia o contador
-	li a2,76 			# Instrumento
+	li a2,2 			# Instrumento
 	li a3,60			# Volume
 
 tocaNA:	beq t0,t2, fimNA		# Termina se alcançar o n de notas
@@ -674,7 +873,7 @@ tocaNA:	beq t0,t2, fimNA		# Termina se alcançar o n de notas
 	lw a1,4(t1)			# Coloca duração	
 	li a7,31		
 	ecall			
-	addi t0,t0,1			
+	addi t0,t0,1	
 	
 	mv a0,a1		
 	li a7,32		
@@ -694,12 +893,6 @@ VITORIA1:
 	li a7, 32
 	ecall				# Pausa o jogo
 	
-	li t2, 0xFF006330
-	la a1, Checkpoint
-	li a4, 56
-	li a6, 8
-	call printUND			# Coloca imagem de checkpoint
-	
 	la t1, PONTUAÇÃO
 	li t3, 0x00000000
 	sw t3, (t1)
@@ -718,6 +911,63 @@ VITORIA1:
 	li t0,0				# Inicia o contador
 	li a2,3 			# Instrumento
 	li a3,60			# Volume
+	
+	li a7, 30
+	ecall
+	add s1, a0, zero
+	add s2, a0, zero
+	li s4, 0
+looptocaVY1:
+	mv s11, t2
+	mv s9, t1
+	mv s8, t0
+	mv s7, a2
+	mv s6, a3
+	
+	li a7, 30
+	ecall
+	bge a0, s2, movecarroVY1
+	j looptocaVY1CONT
+movecarroVY1:
+	li t1, 0xFF000140
+	ble s10, t1, removeCARRO1
+	
+	add t2, s10, zero  		# Coloca valor armazenado em s10 em t2
+	add a3, t2, zero  		# Coloca valor armazenado em t2 em a3
+	la a1, TILE1
+	li a4, 16        		# Define largura da imagem
+	li a6, 16			# Define altura da imagem
+	call printUND			# Remove rastro
+	
+	li t1, -320
+	add s10, s10, t1
+	add t2, s10, zero  		# Coloca valor armazenado em s10 em t2
+	add a3, t2, zero  		# Coloca valor armazenado em t2 em a3
+	lw a1, CARRO
+	li a4, 16        		# Define largura da imagem
+	li a6, 16			# Define altura da imagem
+	call printUND			# Move o carro
+	
+	li a7, 30
+	addi s2, a0, 5		# Demarca velocidade de um pixel por segundo
+looptocaVY1CONT:	
+	beqz s4, looptocaVY1CONT2
+	li t2, 0xFF006330
+	la a1, Checkpoint
+	li a4, 56
+	li a6, 8
+	call printUND			# Coloca imagem de checkpoint
+looptocaVY1CONT2:
+	mv t2, s11
+	mv t1, s9
+	mv t0, s8
+	mv a2, s7
+	mv a3, s6
+	
+	li a7, 30
+	ecall
+	bge a0, s1, tocaVY1
+	j looptocaVY1
 
 tocaVY1:beq t0,t2, fimVY1		# Termina se alcançar o n de notas
 	lw a0,0(t1)			# Coloca nota
@@ -726,18 +976,29 @@ tocaVY1:beq t0,t2, fimVY1		# Termina se alcançar o n de notas
 	ecall			
 	addi t0,t0,1			
 	
-	mv a0,a1		
-	li a7,32		
-	ecall				# Pausa o jogo pela duração
+	mv t3, a1
+	li a7, 30
+	ecall
+	add s1, t3, a0			# Atualiza com a nova duração
 	
 	addi t1,t1,8			# Próx nota/num
-	j tocaVY1			# Volta o loop
+	j looptocaVY1			# Volta o loop
 	
 fimVY1:	li a0, 2000
 	li a7, 32
 	ecall				# Pausa o jogo
 	
 	j COURSE2
+	
+removeCARRO1:
+	add t2, s10, zero  		# Coloca valor armazenado em s10 em t2
+	add a3, t2, zero  		# Coloca valor armazenado em t2 em a3
+	la a1, TILE1
+	li a4, 16        		# Define largura da imagem
+	li a6, 16			# Define altura da imagem
+	call printUND			# Remove rastro
+	li s4, 1
+	j looptocaVY1CONT
 	
 MOEDA1:	li t2, 0xFF002F3C
 	la a1, Coin
@@ -811,3 +1072,55 @@ MOEDA4:	li t2, 0xFF002F60
 	la t2, PONTUAÇÃO
 	sw t1, (t2)
 	j moveCONT5
+	
+	
+	
+	
+VANELLOPE1:
+	la t1, CARRO
+	la t2, CarroV0
+	sw t2, (t1)		# Coloca carro da Vanellope em CARRO
+	
+	la t1, CARROM
+	la t2, CarroVM
+	sw t2, (t1)		# Coloca carro da Vanellope na interface
+	
+	li a7, 31
+	li a0, 67
+	li a1, 1000
+	li a2, 3
+	li a3, 72
+	ecall		#Toca única nota
+	j COURSE1
+GLOYD1:
+	li a7, 31
+	li a0, 67
+	li a1, 1000
+	li a2, 3
+	li a3, 72
+	ecall		#Toca única nota
+	j COURSE1
+SNOWANNA1:
+	la t1, CARRO
+	la t2, CarroS0
+	sw t2, (t1)
+	
+	la t1, CARROM
+	la t2, CarroSM
+	sw t2, (t1)
+	
+	li a7, 31
+	li a0, 67
+	li a1, 1000
+	li a2, 3
+	li a3, 72
+	ecall		#Toca única nota
+	j COURSE1
+LAMAR1:
+	li a7, 31
+	li a0, 67
+	li a1, 1000
+	li a2, 3
+	li a3, 72
+	ecall		#Toca única nota
+	j COURSE1
